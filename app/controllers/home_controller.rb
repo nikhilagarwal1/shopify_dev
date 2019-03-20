2247d8daf4d02bd18cc0e3f8d95ae02bec9b558e
Nikhil Agarwal <nikhil.agarwal0201@gmail.com>
2019-03-20 09:22:34 +0530

    orders and customers are added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    @orders = ShopifyAPI::Order.find(:all, params: { limit: 10 })
+    @customers = ShopifyAPI::Customer.find(:all, params: { limit: 10 })
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,9 +3,9 @@ ShopifyApp.configure do |config|
   config.api_key = "c6bb5d5de4d54b013baadab4648fa8a1"
   config.secret = "25dfc5f6edf6a7a7c025bc6c7f5c33d6"
   config.old_secret = "<old_secret>"
-  config.scope = "read_products" # Consult this page for more scope options:
+  config.scope = "read_products, read_orders, read_customers, write_orders, read_shipping, write_shipping, read_fulfillments, write_fulfillments" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
-  config.embedded_app = true
+  config.embedded_app = false
   config.after_authenticate_job = false
   config.session_repository = Shop
 end
