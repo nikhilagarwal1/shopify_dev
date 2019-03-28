369429786e369d12cf54393f212a96bb285c468d
Nikhil Agarwal <nikhil.agarwal0201@gmail.com>
2019-03-28 14:24:50 +0530

    env file has been added and code optimised

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "c6bb5d5de4d54b013baadab4648fa8a1"
-  config.secret = "25dfc5f6edf6a7a7c025bc6c7f5c33d6"
+  config.api_key = ENV['API_KEY']
+  config.secret = ENV['SECRET']
   config.old_secret = "<old_secret>"
   config.scope = "read_products, read_orders, read_customers, write_orders, read_shipping, write_shipping, read_fulfillments, write_fulfillments" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
